07d724f2eddebe0362e82fe3eba68453dfb6efcd
AntonKechashin <atk@sitecore.net>
2018-09-07 17:31:38 +0300

    Patch for XP 9.0.1 & SXA 1.7.0

---
+++ src/Sitecore.Support.253377/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.253377")]
+[assembly: AssemblyProduct("Sitecore.Support.253377")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.253377/XA/Feature/SiteMetadata/Pipelines/HttpRequestBegin/SitemapHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.HttpRequestBegin
+{
+  using System;
+  using System.Web;
+  using System.Web.Caching;
+
+  public class SitemapHandler : Sitecore.XA.Feature.SiteMetadata.Pipelines.HttpRequestBegin.SitemapHandler
+  {
+    protected new string CacheKey
+    {
+      get
+      {
+        #region Changed code
+        return string.Format("{0}/{1}/{2}/{3}", "XA-SITEMAP", (this.Context.Database == null) ? null : this.Context.Database.Name, this.CurrentSite.Name, HttpContext.Current.Request.Url); 
+        #endregion
+      }
+    }
+
+    protected override string GetSitemapFromCache()
+    {
+      string str = null;
+      if (HttpRuntime.Cache[this.CacheKey] != null)
+      {
+        str = HttpRuntime.Cache.Get(this.CacheKey) as string;
+      }
+      return str;
+    }
+
+    protected override void StoreSitemapInCache(string sitemap, string cacheKey)
+    {
+      HttpRuntime.Cache.Insert(this.CacheKey, sitemap, null, DateTime.UtcNow.AddMinutes((double)this.CacheExpiration), Cache.NoSlidingExpiration);
+    }
+  }
+}
